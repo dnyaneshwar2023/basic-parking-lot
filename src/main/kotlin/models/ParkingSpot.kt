2a9ed59f0ab4eb9c76d73673fcfc38e6fa0a56f2
dnyaneshwar2023 <dnyaneshwarw@sahaj.ai>
2023-02-16 22:46:03 +0530

    Add parking spot class with tests

--- src/main/kotlin/models/ParkingSpot.kt
+++ src/main/kotlin/models/ParkingSpot.kt
@@ -3,12 +3,19 @@ package models
 class ParkingSpot(private val spotID: Int, private var parkingStatus: ParkingStatus) {
     private var vehicle: Vehicle? = null
     fun setVehicle(vehicle: Vehicle) {
+        if (this.vehicle != null) {
+            throw Exception("Vehicle already exists at the spot")
+        }
         this.vehicle = vehicle
         this.parkingStatus = ParkingStatus.PARKED
     }
 
     fun getSpotID(): Int {
         return spotID
+    }
+
+    fun getVehicle(): Vehicle? {
+        return vehicle
     }
 
     fun removeVehicle() {
