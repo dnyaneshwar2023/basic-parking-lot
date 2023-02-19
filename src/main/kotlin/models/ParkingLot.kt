84bd83d6cb8180dd5eef8d690cf62a150456ae81
dnyaneshwar2023 <dnyaneshwarw@sahaj.ai>
2023-02-19 10:33:47 +0530

    Update Date initializations to function parameter

--- src/main/kotlin/models/ParkingLot.kt
+++ src/main/kotlin/models/ParkingLot.kt
@@ -9,6 +9,33 @@ const val PER_HOUR_CHARGE = 10
 class ParkingLot(numberOfSpots: Int) {
     private val spots = MutableList(numberOfSpots) { ParkingSpot(it) }
 
+    fun getSpotById(spotId: Int): ParkingSpot {
+        return spots[spotId]
+    }
+
+    fun parkVehicle(vehicle: Vehicle, entryTime: LocalDateTime): ParkingTicket {
+        if (!isSpotAvailable()) {
+            throw Exception("No Spot available for parking")
+        }
+        val availableSpot = getFirstAvailableSpot()
+        availableSpot.setVehicle(vehicle)
+
+        return TicketGenerator.getTicketFor(availableSpot.getSpotID(), entryTime)
+    }
+
+    fun unparkVehicle(ticket: ParkingTicket, exitTime: LocalDateTime): Receipt {
+        if (spots[ticket.spotID].getVehicle() == null) {
+            throw Exception("No Vehicle Available at the spot")
+        }
+
+        spots[ticket.spotID].removeVehicle()
+        return Receipt(ticket.ticketID, ticket.spotID, ticket.entryTime, exitTime, calculateBill(ticket, exitTime))
+    }
+
+    private fun isSpotAvailable(): Boolean {
+        return spots.any { spot -> spot.isAvailable() }
+    }
+
     private fun calculateBill(ticket: ParkingTicket, exitTime: LocalDateTime): Int {
         val numberOfHours = ticket.entryTime.until(exitTime, ChronoUnit.HOURS).toInt()
         return numberOfHours * PER_HOUR_CHARGE
@@ -17,42 +44,4 @@ class ParkingLot(numberOfSpots: Int) {
     private fun getFirstAvailableSpot(): ParkingSpot {
         return spots.first { spot -> spot.isAvailable() }
     }
-
-    private fun isSpotAvailable(): Boolean {
-        return spots.any { spot -> spot.isAvailable() }
-    }
-
-    fun getSpotById(spotId: Int): ParkingSpot {
-        return spots[spotId]
-    }
-
-    fun parkVehicle(vehicle: Vehicle): ParkingTicket {
-        if (!isSpotAvailable()) {
-            throw Exception("No Spot available for parking")
-        }
-
-        val availableSpot = getFirstAvailableSpot()
-
-        availableSpot.setVehicle(vehicle)
-
-        return TicketGenerator.getTicketFor(availableSpot.getSpotID(), LocalDateTime.now())
-    }
-
-    fun unparkVehicle(ticket: ParkingTicket): Receipt {
-
-        val spotToUnpark = spots[ticket.spotID]
-
-        if (spotToUnpark.getVehicle() == null) {
-            throw Exception("No Vehicle Available at the spot")
-        }
-
-        spotToUnpark.removeVehicle()
-
-        val exitTime = LocalDateTime.now().plusHours(1)
-
-        val billAmount = calculateBill(ticket, exitTime)
-
-        return Receipt(ticket.ticketID, ticket.spotID, ticket.entryTime, exitTime, billAmount)
-    }
-
 }
