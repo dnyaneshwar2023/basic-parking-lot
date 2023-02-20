ca9aa8632905ef45a87a1a7797529ad91114be24
dnyaneshwar2023 <dnyaneshwarw@sahaj.ai>
2023-02-20 16:06:05 +0530

    Update ParkingLotTest
    - Updated tests with calculate bill
    - Removed park and unpark tests (moved to ParkingService)

--- src/test/kotlin/models/ParkingLotTest.kt
+++ src/test/kotlin/models/ParkingLotTest.kt
@@ -8,81 +8,39 @@ import java.time.LocalDateTime
 class ParkingLotTest {
 
     @Test
-    fun `it should park the vehicle to first spot given the spot is available`() {
-        val vehicle = Vehicle(1, VehicleType.CAR)
-        val parkingLot = ParkingLot(5, 1)
+    fun `it should return the first available spot from the parking lot`() {
+        val parkingLot = ParkingLot(1, 1)
 
-        val parkingTicket = parkingLot.parkVehicle(vehicle, LocalDateTime.now())
+        val firstAvailableSpot = parkingLot.getFirstAvailableSpot()
 
-        assertEquals(0, parkingTicket?.spotID)
-        assertEquals(0, parkingTicket?.floorNumber)
-        assertEquals(vehicle, parkingLot.getSpotBySpotNumberAndFloorNumber(0, 0).getVehicle())
+        assertEquals(0, firstAvailableSpot?.getSpotID())
+        assertEquals(0, firstAvailableSpot?.getFloorNumber())
     }
 
     @Test
-    fun `it should park the vehicle to first spot given some vehicles are already parked`() {
-        val vehicleOne = Vehicle(1, VehicleType.CAR)
-        val vehicleTwo = Vehicle(2, VehicleType.CAR)
-        val parkingLot = ParkingLot(5, 1)
-        parkingLot.parkVehicle(vehicleOne, LocalDateTime.now())
+    fun `it should return null if no spot available`() {
+        val parkingLot = ParkingLot(3, 0)
 
-        val parkingTicket = parkingLot.parkVehicle(vehicleTwo, LocalDateTime.now())
+        val firstAvailableSpot = parkingLot.getFirstAvailableSpot()
 
-        assertEquals(0, parkingTicket?.spotID)
-        assertEquals(1,parkingTicket?.floorNumber)
-        assertEquals(vehicleTwo, parkingLot.getSpotBySpotNumberAndFloorNumber(0,1).getVehicle())
+        assertNull(firstAvailableSpot)
     }
 
     @Test
-    fun `it should park the vehicle to first spot given some vehicles are already parked and then unparked`() {
-        val vehicleOne = Vehicle(1, VehicleType.CAR)
-        val vehicleTwo = Vehicle(2, VehicleType.CAR)
-        val vehicleThree = Vehicle(3, VehicleType.CAR)
-        val parkingLot = ParkingLot(5, 1)
-        val firstTicket = parkingLot.parkVehicle(vehicleOne, LocalDateTime.now())
-        parkingLot.parkVehicle(vehicleTwo, LocalDateTime.now())
-        parkingLot.unparkVehicle(firstTicket!!, LocalDateTime.now().plusHours(1))
+    fun `it should calculate the bill for 1 hour`() {
+        val parkingLot = ParkingLot(3, 1)
 
-        val thirdTicket = parkingLot.parkVehicle(vehicleThree, LocalDateTime.now())
-
-        assertEquals(0,thirdTicket?.spotID)
-        assertEquals(0,thirdTicket?.floorNumber)
-        assertEquals(vehicleThree, parkingLot.getSpotBySpotNumberAndFloorNumber(0,0).getVehicle())
+        val billAmount =
+            parkingLot.calculateBill(ParkingTicket(1, 0, 0, LocalDateTime.now()), LocalDateTime.now().plusHours(1))
+        assertEquals(10, billAmount)
     }
 
     @Test
-    fun `it should return null given no free spots are available`() {
-        val vehicle = Vehicle(1, VehicleType.CAR)
+    fun `it should calculate the bill for 1 hour 10 minutes`() {
+        val parkingLot = ParkingLot(3, 1)
 
-        val parkingLot = ParkingLot(0,0)
-
-        assertNull(parkingLot.parkVehicle(vehicle, LocalDateTime.now()))
+        val billAmount =
+            parkingLot.calculateBill(ParkingTicket(1, 0, 0, LocalDateTime.now()), LocalDateTime.now().plusHours(1).plusMinutes(10))
+        assertEquals(20, billAmount)
     }
-
-    @Test
-    fun `it should unpark the vehicle given it was parked on a spot`() {
-        val vehicle = Vehicle(1, VehicleType.CAR)
-        val parkingLot = ParkingLot(5,1)
-        val parkingTicket = parkingLot.parkVehicle(vehicle, LocalDateTime.now())
-
-        val receipt = parkingLot.unparkVehicle(parkingTicket!!, LocalDateTime.now().plusHours(1))
-
-        assertEquals(0, receipt?.spotID)
-        assertEquals(0,receipt?.floorNumber)
-        assertEquals(10, receipt?.bill)
-    }
-
-
-    @Test
-    fun `it should return null while unparking given it was already unparked before`() {
-        val vehicle = Vehicle(1, VehicleType.CAR)
-        val parkingLot = ParkingLot(5,1)
-        val ticket = parkingLot.parkVehicle(vehicle, LocalDateTime.now())
-
-        parkingLot.unparkVehicle(ticket!!, LocalDateTime.now().plusHours(1))
-
-        assertNull(parkingLot.unparkVehicle(ticket, LocalDateTime.now().plusHours(1)))
-    }
-
-
 }
