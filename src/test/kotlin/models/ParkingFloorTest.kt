2c364326996bf6bd439d52db7f67d84a790cca2e
dnyaneshwar2023 <dnyaneshwarw@sahaj.ai>
2023-02-20 11:22:59 +0530

    Create ParkingFloor test with model

---
+++ src/test/kotlin/models/ParkingFloorTest.kt
@@ -0,0 +1,13 @@
+package models
+
+import org.junit.jupiter.api.Test
+
+class ParkingFloorTest {
+
+    @Test
+    fun `it should create new parking floor`() {
+
+        val parkingFloor = ParkingFloor()
+
+    }
+}
