062397c1a5c0fc3a7a82ba3403fb83e331589a11
dnyaneshwar2023 <dnyaneshwarw@sahaj.ai>
2023-02-20 10:04:01 +0530

    Update setVehicle method

--- src/test/kotlin/models/ParkingSpotTest.kt
+++ src/test/kotlin/models/ParkingSpotTest.kt
@@ -1,8 +1,7 @@
 package models
 
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
 
 class ParkingSpotTest {
 
@@ -14,18 +13,6 @@ class ParkingSpotTest {
         parkingSpot.setVehicle(vehicle)
 
         assertEquals(parkingSpot.getVehicle(), vehicle)
-
-    }
-
-    @Test
-    fun `it should throw exception to given spot given spot is not available`() {
-        val vehicleOne = Vehicle(1, VehicleType.CAR)
-        val vehicleTwo = Vehicle(2,VehicleType.CAR)
-
-        val parkingSpot = ParkingSpot(1)
-        parkingSpot.setVehicle(vehicleOne)
-
-        assertThrows<Exception> { parkingSpot.setVehicle(vehicleTwo) }
 
     }
 }
