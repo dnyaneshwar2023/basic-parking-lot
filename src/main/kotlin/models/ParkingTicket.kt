97f2fe9e008303140f933665af5622a88ac522ed
dnyaneshwar2023 <dnyaneshwarw@sahaj.ai>
2023-02-20 13:04:48 +0530

    Remove empty class body

--- src/main/kotlin/models/ParkingTicket.kt
+++ src/main/kotlin/models/ParkingTicket.kt
@@ -2,5 +2,4 @@ package models
 
 import java.time.LocalDateTime
 
-data class ParkingTicket(val ticketID: Int, val spotID: Int, val floorNumber: Int, val entryTime: LocalDateTime) {
-}
+data class ParkingTicket(val ticketID: Int, val spotID: Int, val floorNumber: Int, val entryTime: LocalDateTime)
