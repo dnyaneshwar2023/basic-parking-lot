ce5d5875653b8ea943810478a34b828f142cefc1
dnyaneshwar2023 <dnyaneshwarw@sahaj.ai>
2023-02-20 12:52:21 +0530

    Reorder constructor parameters

--- src/main/kotlin/models/ParkingTicket.kt
+++ src/main/kotlin/models/ParkingTicket.kt
@@ -2,5 +2,5 @@ package models
 
 import java.time.LocalDateTime
 
-data class ParkingTicket(val ticketID: Int, val floorNumber: Int, val spotID: Int, val entryTime: LocalDateTime) {
+data class ParkingTicket(val ticketID: Int, val spotID: Int, val floorNumber: Int, val entryTime: LocalDateTime) {
 }
