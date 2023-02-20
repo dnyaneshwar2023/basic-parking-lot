7acde2f12ba7104c2f39ea36b03ab3e8a6acb877
dnyaneshwar2023 <dnyaneshwarw@sahaj.ai>
2023-02-20 11:55:01 +0530

    Add test with function for getting first available spot

--- src/main/kotlin/models/ParkingFloor.kt
+++ src/main/kotlin/models/ParkingFloor.kt
@@ -3,4 +3,8 @@ package models
 class ParkingFloor(val floorNumber: Int, numberOfSpots: Int) {
     val spots = MutableList(numberOfSpots) { ParkingSpot(it) }
 
+    fun getFirstAvailableSpot(): ParkingSpot? {
+        return spots.firstOrNull() { spot -> spot.isAvailable() }
+    }
+
 }
--- src/test/kotlin/models/ParkingFloorTest.kt
+++ src/test/kotlin/models/ParkingFloorTest.kt
@@ -1,17 +1,37 @@
 package models
 
-import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
 class ParkingFloorTest {
 
+
     @Test
     fun `it should create new parking floor`() {
 
-        val parkingFloor = ParkingFloor(1,5)
+        val parkingFloor = ParkingFloor(1, 5)
 
         assertEquals(parkingFloor.floorNumber, 1)
         assertEquals(5, parkingFloor.spots.size)
 
+    }
+
+    @Test
+    fun `it should return the first available spot given the spot is available`() {
+        val parkingFloor = ParkingFloor(1, 4)
+
+        val firstAvailableSpot = parkingFloor.getFirstAvailableSpot()
+
+        assertNotNull(firstAvailableSpot)
+        assertEquals(0, firstAvailableSpot!!.getSpotID())
+    }
+
+    @Test
+    fun `it should return null given not free spot is available`() {
+        val parkingFloor = ParkingFloor(1,0)
+
+        val firstAvailableSpot = parkingFloor.getFirstAvailableSpot()
+
+        assertNull(firstAvailableSpot)
     }
 }
