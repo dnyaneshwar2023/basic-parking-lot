86eb7ae89a4045984eb91606814bdc3857d88d24
dnyaneshwar2023 <dnyaneshwarw@sahaj.ai>
2023-02-20 10:04:01 +0530

    Update setVehicle method

--- src/main/kotlin/models/ParkingSpot.kt
+++ src/main/kotlin/models/ParkingSpot.kt
@@ -3,9 +3,6 @@ package models
 class ParkingSpot(private val spotID: Int) {
     private var vehicle: Vehicle? = null
     fun setVehicle(vehicle: Vehicle) {
-        if (this.vehicle != null) {
-            throw Exception("Vehicle already exists at the spot")
-        }
         this.vehicle = vehicle
     }
 
