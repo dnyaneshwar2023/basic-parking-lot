5485d9681d36c5899cabdec43ae057717de53dd6
dnyaneshwar2023 <dnyaneshwarw@sahaj.ai>
2023-03-16 15:00:42 +0530

    Add map to add intervals based on vehicle type

--- src/main/kotlin/strategies/FeeCalculationStrategy.kt
+++ src/main/kotlin/strategies/FeeCalculationStrategy.kt
@@ -1,12 +1,13 @@
 package strategies
 
 import models.FeeInterval
+import models.VehicleType
 import java.time.LocalDateTime
 import java.time.temporal.ChronoUnit
 import kotlin.math.min
 
-open class FeeCalculationStrategy(val intervals: ArrayList<FeeInterval>) {
-    open fun getBillAmount(startTime: LocalDateTime, endTime: LocalDateTime): Int {
+open class FeeCalculationStrategy(private val intervals: Map<VehicleType, ArrayList<FeeInterval>>) {
+    open fun getBillAmount(startTime: LocalDateTime, endTime: LocalDateTime, vehicleType: VehicleType): Int {
         val numberOfMinutes = startTime.until(endTime, ChronoUnit.MINUTES)
 
         var numberOfHours = (numberOfMinutes / 60).toInt()
@@ -17,14 +18,19 @@ open class FeeCalculationStrategy(val intervals: ArrayList<FeeInterval>) {
 
         var totalAmount = 0
 
-        for (interval in intervals) {
-            if (interval.start > numberOfHours) {
-                break
+        val chargeIntervals = intervals[vehicleType]
+        if (chargeIntervals != null) {
+            for (interval in chargeIntervals) {
+                if (interval.start > numberOfHours) {
+                    break
+                }
+
+                val chargableHours = min(numberOfHours - interval.start + 1, interval.end - interval.start + 1)
+
+                totalAmount += (chargableHours * interval.charge)
             }
-
-            val chargableHours = min(numberOfHours - interval.start + 1, interval.end - interval.start + 1)
-
-            totalAmount += (chargableHours * interval.charge)
+        } else {
+            throw Exception("Invalid Vehicle Type")
         }
 
         return totalAmount
