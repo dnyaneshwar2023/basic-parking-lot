a033295d997ee0721c84157d87772d3015b7256b
dnyaneshwar2023 <dnyaneshwarw@sahaj.ai>
2023-03-17 13:04:53 +0530

    Update type to support for vehicle types for extraPerDayFee

--- src/main/kotlin/strategies/AirportFeeCalculationStrategy.kt
+++ src/main/kotlin/strategies/AirportFeeCalculationStrategy.kt
@@ -7,13 +7,13 @@ import java.time.LocalDateTime
 
 class AirportFeeCalculationStrategy(
     intervals: Map<VehicleType, ArrayList<FeeInterval>>,
-    private val extraPerDayFee: Int
+    private val extraPerDayFee: Map<VehicleType, Int>
 ) : FeeCalculationStrategy(intervals) {
 
     override fun getBillAmount(startTime: LocalDateTime, endTime: LocalDateTime, vehicleType: VehicleType): Int {
         var totalAmount = 0
         totalAmount += super.getBillAmount(startTime, endTime, vehicleType)
-        totalAmount += (DateTimeUtil.getNumberOfDaysBetween(startTime, endTime) - 1) * extraPerDayFee
+        totalAmount += (DateTimeUtil.getNumberOfDaysBetween(startTime, endTime) - 1) * extraPerDayFee[vehicleType]!!
 
         return totalAmount
     }
